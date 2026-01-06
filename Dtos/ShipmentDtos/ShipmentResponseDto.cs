677567abfa158de7f0311999eb13e55d11cc3167
Ahmed Salah <142540119+Ahmed-Saalah@users.noreply.github.com>
2026-01-06 22:50:02 +0200

    Add ShipmentResponseDto

---
+++ Dtos/ShipmentDtos/ShipmentResponseDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Logex.API.Dtos.ShipmentDtos
+{
+    public class ShipmentResponseDto
+    {
+        public int Id { get; set; }
+        public string TrackingNumber { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public decimal TotalCost { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ShipmentMethod { get; set; } = string.Empty;
+        public string ShipperName { get; set; } = string.Empty;
+        public string ReceiverName { get; set; } = string.Empty;
+    }
+}
